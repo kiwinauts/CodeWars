58652f2686285598766913e90088994918bc756f
John Spantouris <john.spantouris@gmail.com>
2021-11-21 17:41:34 +0200

    Camera Shake

---
+++ CodeWars/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,46 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public Vector3 ShakeDirection;
+
+    public float ShakeDuration;
+
+    public int VibrateStrength;
+
+    [Range(0f,180f)]
+    public float VibrateRandomness;
+
+    public Ease EaseMode;
+
+    private Camera _cameraComponent;
+    
+    private Tweener _shake;
+
+    private void Awake()
+    {
+        _cameraComponent = GetComponent<Camera>();
+    }
+
+    private void Start()
+    {
+        CreateShake();
+    }
+
+    private void CreateShake()
+    {
+        _shake = _cameraComponent.DOShakePosition(ShakeDuration, ShakeDirection, VibrateStrength).SetEase(EaseMode).SetAutoKill(false);
+    }
+
+    private void Update()
+    {
+        Debug.Log(_shake.IsPlaying());
+        if (!_shake.IsPlaying())
+        {
+            CreateShake();
+        }
+    }
+}
